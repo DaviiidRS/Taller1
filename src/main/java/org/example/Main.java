82a4c0346e26f538b3e663d12805f7c3026222bf
David Ramírez <davidramirezbs@gmail.com>
2025-02-18 20:37:23 -0500

    Minor changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Un café ya no es suficiente, necesito masticar un cable electrico 👻");
+        System.out.printf("La fakin maquina de chambear está lista para otro faking día");
     }
 }
