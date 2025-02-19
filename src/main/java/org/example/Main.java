b290c69509cbdab71833c7b2be3ace3b613e32ee
David Ramírez <davidramirezbs@gmail.com>
2025-02-18 20:22:25 -0500

    Commit Inicial

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,6 @@
+package org.example;
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Un café ya no es suficiente, necesito masticar un cable electrico 👻");
+    }
+}
